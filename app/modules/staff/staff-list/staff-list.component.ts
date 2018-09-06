7038c165cddb15e71909f5556ade7068ef78f743
Rajan Vinod Phatak <Rajan_Phatak@ad.infosys.com>
2018-09-06 22:36:31 +0530

    App Availibility

--- app/modules/staff/staff-list/staff-list.component.ts
+++ app/modules/staff/staff-list/staff-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewContainerRef, ChangeDetectorRef } from '@angular/core';
 import { ModalDialogService } from "nativescript-angular/directives/dialogs";
 import { ChangeLocationComponent } from '../../user-control/change-location/change-location.component';
+import { available } from "nativescript-appavailability";
+import { openUrl } from "tns-core-modules/utils/utils";
+import { isIOS } from "platform";
 
 @Component({
     selector: 'staff-list',
@@ -20,15 +23,26 @@ export class StaffListComponent implements OnInit {
 	ngOnInit() { }
 
 	onTapSelector() {
-        let options = {
-            context: {},
-            fullscreen: false,
-            viewContainerRef: this._vcRef,
-            backdrop: "static"
-        };
-        this._modal.showModal(ChangeLocationComponent, options).then(() => {
-            console.log('modal closed');
-        });
-        this._chRef.detectChanges();
+        // let options = {
+        //     context: {},
+        //     fullscreen: false,
+        //     viewContainerRef: this._vcRef,
+        //     backdrop: "static"
+        // };
+        // this._modal.showModal(ChangeLocationComponent, options).then(() => {
+        //     console.log('modal closed');
+        // });
+        // this._chRef.detectChanges();
+        const umaScheme = "uma://";
+        available(umaScheme).then(available => {
+          if (available) {
+            // open in the app
+            openUrl(umaScheme + (isIOS ? "/user?screen_name=" : "user?user_id=") + "eddyverbruggen");
+          } else {
+            // open in the default browser
+            openUrl("https://twitter.com/eddyverbruggen");
+          }
+        })
+
     }
 }
